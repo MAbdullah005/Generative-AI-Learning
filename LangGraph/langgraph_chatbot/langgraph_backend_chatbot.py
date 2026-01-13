bef236eb72eb7e41a112a4f75d762b4bb62b6204
MAbdullah005 <abdullahaliofc@gmail.com>
2026-01-13 05:12:30 -0800

    integrate sqllite

--- LangGraph/langgraph_chatbot/langgraph_backend_chatbot.py
+++ LangGraph/langgraph_chatbot/langgraph_backend_chatbot.py
@@ -3,6 +3,8 @@ from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
+import sqlite3
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langchain_ollama import ChatOllama
 
 
@@ -17,7 +19,9 @@ def chat_node(state: ChatState):
     return {"messages": [response]}
 
 # Checkpointer
-checkpointer = InMemorySaver()
+
+conn=sqlite3.connect(database='chatbot_conv.db',check_same_thread=False)
+checkpointer = SqliteSaver(conn=conn)
 
 graph = StateGraph(ChatState)
 graph.add_node("chat_node", chat_node)
@@ -25,3 +29,13 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
+
+
