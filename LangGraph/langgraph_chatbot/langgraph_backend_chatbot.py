eb2846ad9ac99b84d14fdb73501afefbb4ceaa78
MAbdullah005 <abdullahaliofc@gmail.com>
2026-01-13 04:03:17 -0800

    build memorey element

---
+++ LangGraph/langgraph_chatbot/langgraph_backend_chatbot.py
@@ -0,0 +1,27 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+from langchain_ollama import ChatOllama
+
+
+llm=ChatOllama(model='llama3.2:1b',temperature=0.2,num_predict=256)
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+# Checkpointer
+checkpointer = InMemorySaver()
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
